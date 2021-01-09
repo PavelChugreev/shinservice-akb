9659827d0eaa04611077b39de1f54a5002ae1493
PavelChugreev <chugun92@gmail.com>
2021-01-10 02:20:52 +0300

    create contacts mobile drop menu

--- src/js/script.js
+++ src/js/script.js
@@ -75,14 +75,22 @@ modal.addEventListener("click", (e) => e.target === modal ? closeModal(): null);
 document.addEventListener("keydown", (e) => e.code === "Escape" ? closeModal() : null );
 
 const hamburger = document.querySelector(".hamburger");
+const phoneBtn = document.querySelector(".phone_icon");
 const mobileMenu = document.querySelector(".header_mobile");
-const closeMenulBtn = document.querySelector(".header_mobile_title")
+const mobileContacts = document.querySelector(".header_contacts");
+const closeMenulBtn = document.querySelector(".header_mobile_title i")
+const closeContactslBtn = document.querySelector(".header_contacts_title i")
 
-hamburger.addEventListener("click", () =>{
-    document.querySelector("body").classList.add("body_freeze");
-    mobileMenu.classList.add("show_mobile");
-});
-closeMenulBtn.addEventListener("click", () => {
-    document.querySelector("body").classList.remove("body_freeze");
-    mobileMenu.classList.remove("show_mobile");
-});
+function toggleDropMenus(dropEl, openBtn, closeBtn){
+    openBtn.addEventListener("click", () =>{
+        document.querySelector("body").classList.add("body_freeze");
+        dropEl.classList.add("show_mobile");
+    });
+    closeBtn.addEventListener("click", () => {
+        document.querySelector("body").classList.remove("body_freeze");
+        dropEl.classList.remove("show_mobile");
+    });
+}
+
+toggleDropMenus(mobileMenu, hamburger, closeMenulBtn);
+toggleDropMenus(mobileContacts, phoneBtn, closeContactslBtn);
